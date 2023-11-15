227369de426c8365be0766bb32e9785c2835daf5
hautblis <hautblis@gmail.com>
2023-11-15 22:22:13 +0900

    재고 감소 테스트 (race condition 발생)

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,29 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // Stock 조회
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        System.out.println(stock.getQuantity());
+
+        // 재고를 감소
+        stock.decrease(quantity);
+
+        // 갱신한 값 저장
+        stockRepository.saveAndFlush(stock);
+    }
+}
